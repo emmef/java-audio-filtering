e6950726de04295ceee8750d72fa08cbcc357dfe
Emmef <the.emmef@gmail.com>
2024-12-06 19:54:00 +0100

    BucketScanner groundwork for multi-window measurement

--- emmef-parent/audiobuckets/src/main/java/org/emmef/audio/buckets/BucketScanner.java
+++ emmef-parent/audiobuckets/src/main/java/org/emmef/audio/buckets/BucketScanner.java
@@ -1,42 +1,50 @@
 package org.emmef.audio.buckets;
 
+import lombok.RequiredArgsConstructor;
+
 public class BucketScanner {
 	public static final long MAX_SAMPLE_RATE = 192000;
 	public static final double MAX_WINDOW_SECONDS = 1.0;
 	public static final double MAX_WINDOW_SAMPLES = Math.round(0.5 + MAX_WINDOW_SECONDS * MAX_SAMPLE_RATE);
 	public static final double SCALE = Long.MAX_VALUE / MAX_WINDOW_SAMPLES;
 	public static final LongInteger ZERO = new LongInteger(1);
-	
-	private final long[] bucket;
-	private final double multiplier;
-	private long sampleNumber = 0;
-	private long minimum;
-	private long maximum;
-	private long sum;
 
-	public BucketScanner(double sampleRate, double bucketSeconds) {
+	private final long[] bucket;
+	private long sampleNumber = 0;
+	private double minimum;
+	private double maximum;
+	private double sum;
+	private final Window window[];
+
+	public BucketScanner(double sampleRate, double bucketSeconds, int windows) {
 		long bucketSamples = Math.round(bucketSeconds * sampleRate);
 		if (bucketSamples < 1) {
 			throw new IllegalArgumentException("Invalid bucket size: combination of sample rate (" + sampleRate + ") and seconds (" + bucketSeconds + ") yields zero samples");
-		}
-		else if (bucketSamples > Integer.MAX_VALUE) {
+		} else if (bucketSamples > Integer.MAX_VALUE) {
 			throw new IllegalArgumentException("Invalid bucket size: combination of sample rate (" + sampleRate + ") and seconds (" + bucketSeconds + ") is too large:" + bucketSamples);
 		}
-		int bucketSize = (int)bucketSamples;
+		int bucketSize = (int) bucketSamples;
 		this.bucket = new long[bucketSize];
-		this.multiplier = 1.0 / (SCALE * bucketSize);
+		this.window = new Window[1];
+		this.window[0] = new Window(bucketSize);
 		reset();
 	}
 
+	public BucketScanner(double sampleRate, double bucketSeconds) {
+		this(sampleRate, bucketSeconds, 1);
+	}
+
 	public final void addSample(double sample) {
-		int bucketPosition = (int)(sampleNumber % bucket.length);
-		sampleNumber++;
+		int bucketPosition = (int) (sampleNumber % bucket.length);
 		long quantizedSample = Math.round(sample * sample * SCALE);
 
-		final long oldestSample = bucket[bucketPosition];
-		bucket[bucketPosition] = quantizedSample;
-		sum -= oldestSample;
-		sum += quantizedSample;
+		sum = this.window[0].addAndGet(bucketPosition, quantizedSample);
+		for (int i = 1; i < window.length; i++) {
+			sum = Math.max(sum, this.window[i].addAndGet(bucketPosition, quantizedSample));
+		}
+
+		this.bucket[bucketPosition] = quantizedSample;
+		sampleNumber++;
 
 		if (isWholeBucketScanned()) {
 			minimum = Math.min(minimum, sum);
@@ -44,42 +52,65 @@ public class BucketScanner {
 		}
 	}
 
-	private long getSumOfScaledSquared() {
-		return sum;
-	}
-
 	public void reset() {
 		sampleNumber = 0;
 		sum = 0;
-		minimum = Long.MAX_VALUE;
-		maximum = Long.MIN_VALUE;
+		minimum = Double.MAX_VALUE;
+		maximum = Double.MIN_VALUE;
 		for (int i = 0; i < bucket.length; i++) {
 			bucket[i] = 0;
 		}
 	}
-	
+
 	public boolean isWholeBucketScanned() {
 		return sampleNumber >= bucket.length;
 	}
-	
+
 	public int getBucketSize() {
 		return bucket.length;
 	}
-	
+
 	public double getMinimum() {
-		return multiplier * minimum;
+		return minimum;
 	}
 
 	public double getMaximum() {
-		return multiplier * maximum;
+		return maximum;
 	}
 
 	public double getMeanSquared() {
-		return multiplier * getSumOfScaledSquared();
+		return sum;
 	}
 
 	public double getRootMeanSquared() {
 		return Math.sqrt(getMeanSquared());
 	}
 
+	public double getValue() {
+		return getRootMeanSquared();
+	}
+
+	private class Window {
+		private final int samples;
+		private final double scale;
+		private long sum;
+
+		Window(int windowSize) {
+			this.samples = Math.min(windowSize, bucket.length);
+			final double weight = Math.pow(1.0 * this.samples / bucket.length, 0.25);
+			this.scale = weight / (SCALE * this.samples);
+			this.sum = 0;
+		}
+
+		double addAndGet(int bucketPosition, long newValue) {
+			int oldestSamplePosition = (bucketPosition + bucket.length - this.samples) % bucket.length;
+			this.sum -= bucket[oldestSamplePosition];
+			this.sum += newValue;
+			return this.scale * this.sum;
+		}
+
+		void reset() {
+			this.sum = 0;
+		}
+	}
 }
